71edbc31c9a9e2b958629630485510e598e1b7d5
Yerayz <redyerayz@gmail.com>
2022-12-30 15:50:54 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+function main(){
+    var canvas = document.getElementById("myCanvas");
+    var gl = canvas.getContext("webgl");
+
+    triangle = [
+        0.0, 0.5,
+        0.0, 0.0,
+        0.5, 0.0
+    ];
+
+    var vertexShaderCode = document.getElementById("vertexShaderCode").text;
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vertexShaderCode);
+    gl.compileShader(vertexShader);
+
+    var fragmentShaderCode = document.getElementById("fragmentShaderCode").text;
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fragmentShaderCode);
+    gl.compileShader(fragmentShader);
+
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    var positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangle), gl.STATIC_DRAW);
+
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.useProgram(program);
+
+    gl.enableVertexAttribArray(positionAttributeLocation);
+    gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+}
